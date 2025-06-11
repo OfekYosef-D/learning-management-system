601dff3ba171f2542152892d322026fde1ab0125
OfekYosef-D <myofek1@gmail.com>
2025-06-11 11:50:52 +0300

    Added docker

--- server/dist/index.js
+++ server/dist/index.js
@@ -32,11 +32,20 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.clerkClient = void 0;
+exports.handler = exports.clerkClient = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
@@ -44,6 +53,8 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const dynamoose = __importStar(require("dynamoose"));
+const serverless_http_1 = __importDefault(require("serverless-http"));
+const seedDynamodb_1 = __importDefault(require("./seed/seedDynamodb"));
 const express_2 = require("@clerk/express");
 /* ROUTE IMPORTS */
 const courseRoutes_1 = __importDefault(require("./routes/courseRoutes"));
@@ -83,3 +94,18 @@ if (!isProduction) {
         console.log(`Server is running on port:${port}`);
     });
 }
+// aws proudction environment
+const serverlessApp = (0, serverless_http_1.default)(app);
+const handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
+    if (event.action === "seed") {
+        yield (0, seedDynamodb_1.default)();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Database seeded successfully' }),
+        };
+    }
+    else {
+        return serverlessApp(event, context);
+    }
+});
+exports.handler = handler;
